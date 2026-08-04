f3c4d3bf7773bfeb25dd9573b1dc109faa12b0a8
Michael <dementiev.m.e@yandex.ru>
2026-08-04 19:48:31 +0300

    Added usefull comments in code and license headers.

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,3 +1,9 @@
+// Copyright (c) 2026 Michael_Go. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
+// Package plugin provides the golangci-lint v2 module plugin entry point
+// for the loglinter analyzer.
 package plugin
 
 import (
@@ -6,10 +12,14 @@ import (
 	"github.com/dementievme/golangci-lint-plugin/internal/analyzer"
 )
 
+// plugin implements the golangci-lint AnalyzerPlugin interface.
 type plugin struct{}
 
+// GetAnalyzers returns the list of analyzers provided by this plugin.
 func (plugin) GetAnalyzers() []*analysis.Analyzer {
 	return []*analysis.Analyzer{analyzer.New()}
 }
 
+// AnalyzerPlugin is the exported symbol that golangci-lint discovers
+// when loading this module as a custom linter plugin.
 var AnalyzerPlugin plugin
